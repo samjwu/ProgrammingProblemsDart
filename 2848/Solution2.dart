cb6e1128d634dbe459a10dc5550539434fcc3417
samjwu <sjwu@ualberta.ca>
2023-10-14 18:33:54 -0600

    2848 2

---
+++ 2848/Solution2.dart
@@ -0,0 +1,18 @@
+class Solution {
+    int numberOfPoints(List<List<int>> nums) {
+        var seenPoints = <int>{};
+        
+        int ans = 0;
+        
+        for (List<int> car in nums) {
+            for (int i = car[0]; i <= car[1]; i++) {
+                if (seenPoints.contains(i) == false) {
+                    seenPoints.add(i);
+                    ans += 1;
+                }
+            }
+        }
+        
+        return ans;
+    }
+}
